b82672653eb4aa2305c208705fbf3566f172f89c
James Michael DuPont <jamesmikedupont@gmail.com>
2016-03-04 22:24:38 +0000

    updates

--- process.sh
+++ process.sh
@@ -1,3 +1,5 @@
+
+rm kosovo-latest.osm.bz2
 wget http://download.geofabrik.de/europe/kosovo-latest.osm.bz2
 bzcat kosovo-latest.osm.bz2  | perl findnames.pl   > report.md
 bzcat kosovo-latest.osm.bz2  | perl find_in_serbia.pl > in_serbia.md
